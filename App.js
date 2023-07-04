858e9c3d565339df9acd8b3aa52dc42c809b976c
Edmond <mredmondwang@gmail.com>
2023-07-05 00:22:33 +0400

    Finished delivery screen and the app.

--- App.js
+++ App.js
@@ -23,16 +23,20 @@ export default function App() {
         <Stack.Navigator>
           <Stack.Screen name="Home" component={HomeScreen} />
           <Stack.Screen name="Restaurant" component={RestaurantScreen} />
-          <Stack.Screen 
-            name="Basket" 
-            component={BasketScreen} 
-            options={{ presentation: 'modal', headerShown: false}}
+          <Stack.Screen
+            name="Basket"
+            component={BasketScreen}
+            options={{ presentation: "modal", headerShown: false }}
           />
-          <Stack.Screen name='PreparingOrderScreen' component={PreparingOrderScreen} 
-            options={{presentation: 'fullScreenModal', headerShown: false}}
+          <Stack.Screen
+            name="PreparingOrderScreen"
+            component={PreparingOrderScreen}
+            options={{ presentation: "fullScreenModal", headerShown: false }}
           />
-          <Stack.Screen name='Delivery' component={DeliveryScreen} 
-            options={{presentation: 'fullScreenModal', headerShown: false}}
+          <Stack.Screen
+            name="Delivery"
+            component={DeliveryScreen}
+            options={{ presentation: "fullScreenModal", headerShown: false }}
           />
         </Stack.Navigator>
       </Provider>
--- screens/PreparingOrderScreen.js
+++ screens/PreparingOrderScreen.js
@@ -1,38 +1,37 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import * as Animatable from 'react-native-animatable'; 
-import * as Progress from 'react-native-progress'
-import { useNavigation } from '@react-navigation/native';
+import { View, Text } from "react-native";
+import React, { useEffect } from "react";
+import * as Animatable from "react-native-animatable";
+import * as Progress from "react-native-progress";
+import { useNavigation } from "@react-navigation/native";
 
 const PreparingOrderScreen = () => {
-  
-  const navigation= useNavigation();
+  const navigation = useNavigation();
 
   useEffect(() => {
     setTimeout(() => {
       navigation.navigate("Delivery");
     }, 4000);
-  }, [])
+  }, []);
   return (
-    <View className='bg-[#00CCBB] flex-1 justify-center items-center'>
-      <Animatable.Image 
-        source={require('../assets/orderLoading.gif')}
-        animation='slideInUp'
+    <View className="bg-[#00CCBB] flex-1 justify-center items-center">
+      <Animatable.Image
+        source={require("../assets/orderLoading.gif")}
+        animation="slideInUp"
         iteractionCount={1}
-        className='h-96 w-96'
+        className="h-96 w-96"
       />
 
       <Animatable.Text
-        animation='slideInUp'
+        animation="slideInUp"
         iteractionCount={1}
-        className='text-lg my-10 text-white font-bold text-center'>
+        className="text-lg my-10 text-white font-bold text-center"
+      >
         Waiting for Restaurant to {"\n"} accept your order!
       </Animatable.Text>
 
-      <Progress.Bar width={300} progress={0.8} color={'white'}/>
-      
+      <Progress.Bar width={300} indeterminate={true} color={"white"} />
     </View>
-  )
-}
+  );
+};
 
-export default PreparingOrderScreen
+export default PreparingOrderScreen;
